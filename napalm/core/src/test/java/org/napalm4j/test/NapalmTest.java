932b8f7ffc0e0a884b5c2936f98216a4d5c766e2
Jacek Furmankiewicz <jacek99@gmail.com>
2011-01-27 18:38:16 -0500

    docs

---
+++ napalm/core/src/test/java/org/napalm4j/test/NapalmTest.java
@@ -0,0 +1,27 @@
+package org.napalm4j.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.napalm4j.Napalm;
+import org.springframework.stereotype.Service;
+
+/**
+ * Unit test for simple App.
+ */
+@Service @Path("/") 
+public class NapalmTest  {
+
+    @GET @Produces(MediaType.TEXT_PLAIN)
+    public String get() {
+        return "hi";
+    }
+
+    public static void main( String[] args )
+    {
+        Napalm.run(8080,NapalmTest.class);
+    }
+    
+}
