1039b34924f57dd13633d5202a7af625f363419f
Jacek Furmankiewicz <jacek99@gmail.com>
2011-01-29 09:53:04 -0500

    logging improvements

---
+++ napalm/core/src/main/java/com/github/napalm/spring/NapalmServer.java
@@ -0,0 +1,61 @@
+package com.github.napalm.spring;
+
+import java.util.Set;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
+
+@Service
+public class NapalmServer extends Server {
+
+	private static final Logger LOG = LoggerFactory.getLogger(NapalmServer.class);
+	
+	@Autowired
+	private WebApplicationContext ctx;
+	@Autowired
+	private NapalmConfig config;
+
+	public void init(int port, Class<?>... apps) {
+
+		Connector connector = new SelectChannelConnector();
+		connector.setPort(port);
+		setConnectors(new Connector[] { connector });
+
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		context.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, ctx);
+
+		SpringServlet servlet = new SpringServlet();
+		ServletHolder holder = new ServletHolder(servlet);
+		holder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass",
+				"com.sun.jersey.api.core.PackagesResourceConfig");
+
+		// auto-register all JAX-RS classes
+		Set<String> packages = Sets.newHashSet();
+		for (Class<?> app : apps) {
+			packages.add(app.getPackage().getName());
+		}
+		holder.setInitParameter("com.sun.jersey.config.property.packages", Joiner.on(",").join(packages));
+
+		context.addServlet(holder, "/*");
+
+		setHandler(context);
+		
+		if (config.isInDevelopmentMode()) {
+			LOG.info("Detected Napalm DEVELOPMENT mode");
+		}
+	}
+
+}
